6848a4a5b7f40a0448ef34be59c49b9bd970604c
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-05 17:42:50 +0100

    Dodaj pliki projektów.

---
+++ WWWSSE2021PP01_6679/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+    Napisz program, który pyta użytkownika o podanie liczby całkowitych i w wyniku wyświetla
+    informacje o parzystości lub nieparzystości.
+*/
+
+Console.WriteLine("Podaj jakąś liczbę całkowitą: ");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine("> Podana liczba jest parzysta.");
+    else Console.WriteLine("> Podana liczba nie jest parzysta.");
